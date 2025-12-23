166c027f8df8e9d15beb6f228305447abe6d60e9
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2025-12-23 15:34:16 +0000

    2018 Day 15 (what a pain)

--- src/grid.rs
+++ src/grid.rs
@@ -2,16 +2,20 @@ pub fn four_neighbours<T>((r, c): (usize, usize), grid: &Vec<Vec<T>>) -> Vec<(us
     let mut res = Vec::new();
     if r > 0 { res.push((r - 1, c)); }
     if c > 0 { res.push((r, c - 1)); }
-    if r < grid.len() - 1 { res.push((r + 1, c)); }
     if c < grid[r].len() - 1 { res.push((r, c + 1)); }
+    if r < grid.len() - 1 { res.push((r + 1, c)); }
     res
 }
 
 pub fn eight_neighbours<T>((r, c): (usize, usize), grid: &Vec<Vec<T>>) -> Vec<(usize, usize)> {
-    let mut res = four_neighbours((r, c), grid);
+    let mut res = Vec::new();
     if r > 0 && c > 0 { res.push((r - 1, c - 1)); }
+    if r > 0 { res.push((r - 1, c)); }
     if r > 0 && c < grid[r - 1].len() - 1 { res.push((r - 1, c + 1)); }
+    if c > 0 { res.push((r, c - 1)); }
+    if c < grid[r].len() - 1 { res.push((r, c + 1)); }
     if r < grid.len() - 1 && c > 0 { res.push((r + 1, c - 1)); }
+    if r < grid.len() - 1 { res.push((r + 1, c)); }
     if r < grid.len() - 1 && c < grid[r + 1].len() - 1 { res.push((r + 1, c + 1)); }
     res
 }
